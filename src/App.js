2cc4376034f5a3b116d1682bbb571c0f5fea1d10
Ryan <straightwind0726@gmail.com>
2023-08-13 19:29:44 +0900

    [Update] Basic Settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { styled } from 'styled-components';
 import './CSS/index.css';
+import Header from './Components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Movie from './Pages/Movie';
+import Tv from './Pages/Tv';
+import Search from './Pages/Search';
+import NotFound from './Pages/NotFound';
+
+const MainWrapper = styled.div``;
 
 function App() {
-    return <div>hi</div>;
+    return (
+        <MainWrapper>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Movie />} />
+                <Route path="/tv" element={<Tv />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </MainWrapper>
+    );
 }
 
 export default App;
