24286a96c7e3f0b59ed4da8416531b8b31eb8d7a
Ryan <straightwind0726@gmail.com>
2023-08-13 20:43:07 +0900

    [Update] Header

--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { styled } from 'styled-components';
+
+const MovieWrapper = styled.div`
+    padding: 120px 0;
+`;
 
 export default function Movie() {
-    return <div>Movie</div>;
+    return <MovieWrapper>Movie</MovieWrapper>;
 }
