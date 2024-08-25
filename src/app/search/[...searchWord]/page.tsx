36df2afdd83fd575861afe16d65eb9db3937f045
Ryan <straightwind0726@gmail.com>
2024-08-25 18:21:46 +0900

    [Update] 헤더 : 검색란 추가 & 검색 페이지 추가

---
+++ src/app/search/[...searchWord]/page.tsx
@@ -0,0 +1,57 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { AccentTextColor, getSearch } from '@/service/common';
+import { getImages } from '@/utils/getImage';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface SearchPageProps {
+  params: {
+    searchWord: string[];
+  };
+}
+
+export default async function SearchPage({ params }: SearchPageProps) {
+  const searchKey = decodeURIComponent(params.searchWord[0]);
+  const data = (await getSearch(searchKey)).results;
+  const getLink = (media_type: string, id: number) => {
+    return media_type === 'movie' ? `/movie/detail/${id}` : `/tv/detail/${id}`;
+  };
+
+  return (
+    <div className="flex flex-col bg-black">
+      <h2 className="text-center text-2xl font-bold italic pt-24 pb-12">
+        {data.length > 0
+          ? `[ ${searchKey} ]로 검색한 결과 입니다.`
+          : `[ ${searchKey} ]로 검색한 결과가 없습니다.`}
+      </h2>
+      <div className="grid grid-cols-5 gap-4 px-24 pb-24">
+        {data.length > 0 &&
+          data.map((item: any) => {
+            return (
+              <Card key={item.id}>
+                <CardContent className="group relative flex aspect-square items-center justify-center p-2 origin-bottom duration-300 hover:-translate-y-4">
+                  <Link href={getLink(item.media_type, item.id)}>
+                    <Image
+                      layout="fill"
+                      className="rounded-md group-hover:opacity-15"
+                      src={getImages(item.poster_path || item.backdrop_path)}
+                      alt={item.name}
+                    />
+                    <div className="absolute text-white top-0 left-0 w-full h-full pt-4 px-4 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100">
+                      <h4
+                        className="text-xl"
+                        style={{ textShadow: AccentTextColor }}
+                      >
+                        {item.name}({item.vote_average.toFixed(1)})
+                      </h4>
+                    </div>
+                  </Link>
+                </CardContent>
+              </Card>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -1,13 +1,25 @@
 'use client';
 
+import { Check, Search } from 'lucide-react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { useState } from 'react';
 
 export function Navigation() {
   const pathName = usePathname();
+  const router = useRouter();
+
+  const [searchWord, setSearchWord] = useState('');
+  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push(`/search/${searchWord}`);
+    setSearchWord('');
+  };
 
   return (
-    <nav className="w-full p-2 absolute top-0 left-0 z-50">
+    <nav className="w-full p-2 absolute top-0 left-0 z-50 flex items-center justify-between">
       <ul className="w-full h-12 flex items-center text-sm">
         <li>
           <Link
@@ -39,6 +51,20 @@ export function Navigation() {
           <Link href="/tv">TV SHOWS</Link>
         </li>
       </ul>
+      <form onSubmit={handleSubmit} className="flex items-center">
+        <Search className="w-12" />
+        <Input
+          className="mx-2 text-black"
+          type="text"
+          value={searchWord}
+          onChange={(e) => {
+            setSearchWord(e.target.value);
+          }}
+        />
+        <Button className="bg-transparent">
+          <Check />
+        </Button>
+      </form>
     </nav>
   );
 }
